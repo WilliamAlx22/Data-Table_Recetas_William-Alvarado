b5585a36974eb537f93c7de714854e7f26650edd
William Alvarado <115381750+WilliamAlx22@users.noreply.github.com>
2022-10-22 18:36:21 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+function Receta1() {
+        
+    Swal.fire({
+        imageUrl: 'img/Receta1.jpg',
+        imageWidth: 1200,
+        imageAlt: 'WilliamAlvarado'
+    })
+    
+}
+function Receta2() {
+        
+  Swal.fire({
+      imageUrl: 'img/Receta2.jpg',
+      imageWidth: 1200,
+      imageAlt: 'WilliamAlvarado'
+  })
+  
+}
+function Receta3() {
+        
+  Swal.fire({
+      imageUrl: 'img/Receta3.jpg',
+      imageWidth: 1200,
+      imageAlt: 'WilliamAlvarado'
+  })
+  
+}
+function Favoritos() {
+
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Guardado en favoritos',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    
+}
+/*By William Alvarado - 61-1529-2021*/
+function Descargar() {
+
+  Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Receta descargada con exito!!!',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  
+}
